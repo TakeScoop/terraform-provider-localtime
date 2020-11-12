42f5fe3c1eb6d2f0625e6e97c491403590d9fdea
Bo Huang <beyondbill@users.noreply.github.com>
2020-11-12 11:32:31 -0800

    schema (#2)

---
+++ go.mod
@@ -0,0 +1,8 @@
+module terraform-provider-localtime
+
+go 1.15
+
+require (
+	github.com/araddon/dateparse v0.0.0-20201001162425-8aadafed4dc4
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.2.0
+)
---
+++ localtime/data_source_timestamp.go
@@ -0,0 +1,77 @@
+package localtime
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/araddon/dateparse"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceTimestamp() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceTimestampRead,
+		Schema: map[string]*schema.Schema{
+			"layout": &schema.Schema{
+				Type:          schema.TypeString,
+				Optional:      true,
+				ConflictsWith: []string{"layout_timezone"},
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(string)
+					t := time.Now()
+
+					if v == t.Format(v) {
+						errs = append(errs, fmt.Errorf("'%s' is not a valid time layout!", v))
+					}
+					return
+				},
+			},
+			"layout_timezone": &schema.Schema{
+				Type:          schema.TypeString,
+				Optional:      true,
+				Default:       "-0700",
+				ConflictsWith: []string{"layout"},
+			},
+			"local_time": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(string)
+					_, err := dateparse.ParseStrict(v)
+					if err != nil {
+						errs = append(errs, err)
+					}
+					return
+				},
+			},
+			"location": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(string)
+					_, err := time.LoadLocation(v)
+					if err != nil {
+						errs = append(errs, err)
+					}
+					return
+				},
+			},
+			"timestamp": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceTimestampRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	// Add implementation details here
+
+	return diags
+}
